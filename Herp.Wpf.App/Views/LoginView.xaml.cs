f04193814a4e6cb9c207b7e73e68c2c8794cf746
HugoMGSung <personar95@naver.com>
2020-09-27 10:34:30 +0900

    Hugo ERP OnAndOn

---
+++ Herp.Wpf.App/Views/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using MahApps.Metro.Controls;
+
+namespace Herp.Wpf.App.Views
+{
+    /// <summary>
+    /// LoginView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginView : MetroWindow
+    {
+        private readonly MainView _parentView;
+
+        public LoginView(MainView parentView)
+        {
+            InitializeComponent();
+            _parentView = parentView;
+
+            Closing += OnClosing;
+            Closed += OnClosed;
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            _parentView.IsActiveWin = true;
+        }
+    }
+}
---
+++ Herp.Wpf.App/Views/MainView.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using MahApps.Metro.Controls;
+
+namespace Herp.Wpf.App.Views
+{
+    /// <summary>
+    /// MainView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        private enum FadeDirection
+        {
+            FadeIn,
+            FadeOut
+        }
+
+        private readonly TransparentAdorner _windowAdorner;
+
+        private bool _isActiveWin = true;
+        public bool IsActiveWin
+        {
+            get => _isActiveWin;
+
+            set
+            {
+                if (_isActiveWin == value) return;
+                _isActiveWin = value;
+
+                if (_isActiveWin)
+                    DettachWindowAdorner();
+                else
+                    AttachWindowAdorner();
+            }
+        }
+
+        private void AttachWindowAdorner()
+        {
+            var parentAdorner = AdornerLayer.GetAdornerLayer(borderWindow);
+            parentAdorner?.Add(_windowAdorner);
+
+            FadeAnimation(borderWindow, FadeDirection.FadeIn);
+        }
+
+        private void DettachWindowAdorner()
+        {
+            FadeAnimation(borderWindow, FadeDirection.FadeOut);
+
+            AdornerLayer parentAdorner = AdornerLayer.GetAdornerLayer(borderWindow);
+            parentAdorner?.Remove(_windowAdorner);
+        }
+
+        private void FadeAnimation(Border border, FadeDirection fadeDirection)
+        {
+            var animFade = new DoubleAnimation();
+
+            if (fadeDirection == FadeDirection.FadeIn)
+            {
+                animFade.From = 0;
+                animFade.To = .10;
+            }
+            else
+            {
+                animFade.From = .10;
+                animFade.To = 0;
+            }
+
+            animFade.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+            var brush = new SolidColorBrush { Color = Colors.Black };
+            brush.BeginAnimation(Brush.OpacityProperty, animFade);
+            border.Background = brush;
+        }
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            _windowAdorner = new TransparentAdorner(borderWindow);
+
+            Loaded += OnLoaded;
+            ContentRendered += OnContentRendered;
+        }
+
+        private void OnContentRendered(object sender, EventArgs e)
+        {
+            LoginView childWindow = new LoginView(this);
+            IsActiveWin = false;
+            childWindow.Owner = this;
+            IsEnabled = false;
+            childWindow.ShowDialog();
+            IsEnabled = true;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
